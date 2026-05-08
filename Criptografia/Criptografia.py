7b2fd979511a4ce1457fd5df7d4be91f62f98aea
CaioHSilvaMC <caiohsilvamc@gmail.com>
2026-05-07 21:58:17 -0300

    Modulo A criado

--- Criptografia/Criptografia.py
+++ Criptografia/Criptografia.py
@@ -1,29 +1,86 @@
-from sympy import isprime
-from Funcoes import *
+from Funcoes import defina_d, criptografar, criarSequencia
+from sympy import isprime 
+from sympy import randprime
 
-p = int(input("Digite o valor de P: "))
-q = int(input("Digite o valor de Q: "))
+#Escolher como gerar P e Q
+print("Escolha como será gerado P e Q:")
+op = int(input("1. Manualmente (Você escolhe os valores)\n" \
+               "2. Aleatoriamente (O sistema gera os valores)\n"))
 
-if isprime(p) and isprime(q):
+if op == 1:
+    frase = input("Digite a frase que será criptografada: \n")
+
+    #Verifica se P e Q são primos
+    while True:
+        p = int(input("Escolha o valor de P: "))
+        q = int(input("Escolha o valor de Q: "))
+
+        #Função para verificar se são primos e diferentes
+        if isprime(p) and isprime(q) and p != q:
+            break
+        else:
+            print("Ambos precisam ser primos e distintos, escolha outros valores...")
+
+    #Calcula os valores N e Z
     n = p*q
     z = (p-1)*(q-1)
 
     print ("Valor de Z: ", z)
-    d = int(input("Digite o Valor de D (Precisa ser Primo em relação a Z): "))
+    #Chama a função para gerar o D
+    d = defina_d(z)
 
-    if mdc(z, d) == 1:
-        e = pow(d, -1, z)
+    #Calculo para encontrar o valor de E (Pesquisei como fazer o cálculo de E)
+    e = pow(d, -1, z)
 
-        print ("Valor de P", p)
-        print ("Valor de Q", q)
-        print ("Valor de N", n)
-        print ("Valor de Z", z)
-        print ("Valor de D", d)
-        print ("Valor de E", e)
-    else:
-        print ("D precisa ser um coprimo de Z")
+    print("Chave Pública (N e E): ", n, " e ", e)
+    print("Chave Privada (N e D): ", n, " e ", d)
+
+    #Chama a função para transformar em uma sequência numérica em ASCII
+    textoCriptografado = criptografar(frase, e, n)
+
+    #Chama a função que torna o vetor em uma sequência numérica
+    textoFinal = criarSequencia(textoCriptografado)
+
+    #Exibe resultado final
+    print("Texto criptografado: ", textoFinal)
+
+elif op == 2:
+    frase = input("Digite a frase que será criptografada: \n")
+
+    #Gera números primos aleatórios 
+    print("Gerando aleatoriamente...")
+    while True:
+        p = randprime(1000,100000)
+        q = randprime(1000,100000)
+
+        if p != q:
+            break
+
+    print("Valor de P: ", p)
+    print("Valor de Q: ", q)
+
+    #Calcula os valores N e Z
+    n = p*q
+    z = (p-1)*(q-1)
+
+    print ("Valor de Z: ", z)
+    #Chama a função para gerar o D
+    d = defina_d(z)
+
+    #Calculo para encontrar o valor de E (Pesquisei como fazer o cálculo de E)
+    e = pow(d, -1, z)
+
+    print("Chave Pública (N e E): ", n, " e ", e)
+    print("Chave Privada (N e D): ", n, " e ", d)
+
+    #Chama a função para transformar em uma sequência numérica em ASCII
+    textoCriptografado = criptografar(frase, e, n)
+
+    #Chama a função que torna o vetor em uma sequência numérica
+    textoFinal = criarSequencia(textoCriptografado)
+
+    #Exibe resultado final
+    print("Texto criptografado: ", textoFinal)
+
 else:
-    print ("Ambos, P e Q, precisam ser primos")
-
-
-
+    print("Opção Inválida...")
--- Criptografia/Funcoes.py
+++ Criptografia/Funcoes.py
@@ -1,4 +1,37 @@
+import random
+
+#Calcula o MDC de dois números
 def mdc (z, d):
     while d != 0:
         z, d = d, z %d
     return z
+
+#Gera um valor aleatório para D
+def defina_d (z):
+    while True:
+        d = random.randint(1,100000)
+
+        if mdc(z, d) == 1:
+            return d
+
+#Pega uma frase, coloca na tabela ASCII e criptogtafa em RSA 
+def criptografar(frase, e, n):
+    textoCriptografado = []
+
+    for letra in frase:
+        fraseAscii = ord(letra)
+        #Pesquisei como fazer essa conta em Python
+        criptografado = pow(fraseAscii, e, n)
+
+        textoCriptografado.append(criptografado)
+
+    return textoCriptografado
+
+#Transforma o Vetor em uma sequência de números
+def criarSequencia(textoC):
+    textoFinal = ""
+
+    for num in textoC:
+        textoFinal += str(num)
+
+    return textoFinal
